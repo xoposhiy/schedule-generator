9f9f479e5c3b65960ae5fd675870687646446fbf
Teewar <teewar25@gmail.com>
2020-11-06 10:50:36 +0500

    add Rule

---
+++ ScheduleGenerator/ScheduleLib/Rule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScheduleLib
+{
+    class Rule
+    {
+        public int Weight;
+        public Func<Schedule, IEnumerable<Meeting>> GetConflictingMeetings;
+
+        public Rule(Func<Schedule, IEnumerable<Meeting>> getConflictingMeetings, int weight)
+        {
+            GetConflictingMeetings = getConflictingMeetings;
+            Weight = weight;
+        }
+    }
+}
