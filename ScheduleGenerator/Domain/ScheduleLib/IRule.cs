9302fd85e1628c7c9ac4a17eb91442521f503824
Teewar <teewar25@gmail.com>
2020-12-08 09:57:45 +0500

    add some Rules

---
+++ ScheduleGenerator/Domain/ScheduleLib/IRule.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.ScheduleLib
+{
+    public class EvaluationResult
+    {
+        public double Penalty;
+        public Meeting[] BadMeetings;
+        public string PenaltyDescription;
+        
+        public EvaluationResult(double penalty, Meeting[] badMeetings, string penalityDescription)
+        {
+            Penalty = penalty;
+            BadMeetings = badMeetings;
+            PenaltyDescription = penalityDescription;
+        }
+    }
+
+    public interface IRule
+    {
+         EvaluationResult Evaluate(Schedule schedule);
+    }
+}
--- ScheduleGenerator/Domain/ScheduleLib/Requisition.cs
+++ ScheduleGenerator/Domain/ScheduleLib/Requisition.cs
@@ -1,31 +1,18 @@
-﻿using System.Linq;
-
-namespace ScheduleLib
+﻿namespace Domain.ScheduleLib
 {
     public class Requisition
     {
         public LearningPlanItem PlanItem;
-        
-        /* TODO: Тут нужно, чтобы в коде было понятно разделены следующие кейсы:
-        "ФИИТ-101, ФИИТ-102" — означает, что эти две группы имеют одинаковый приоритет
-        "ФИИТ-101+ФИИТ-102 — означает, что это поток из двух групп (видимо, лекционный).
-        "ФИИТ-101 + ФИИТ-102, ФИИТ-103 + ФИИТ-104 — означает, что это либо поток из 101,102, либо поток из 103,104 и они имеют один приоритет. Тут логичнее приоритет сделать больше именно у +.
-        "ФИИТ-1*+ФИИТ-1*" означает, что это поток из любых двух групп ФИИТа будет эквивалентен по приоритету.
-
-        Надо убрать парсинг и сплит по плюсикам, а сделать объектную модель, которая представляет всё это многообразие вариантов.
-         */
         public GroupRequisition[] GroupPriorities;
 
         //TODO: Нужно более строго смоделировать, что это либо комп-класс, либо класс с проектором, либо конкретный номер аудитории.
-        public string? Location;
+        public string Location;
         public int RepetitionsCount;
         
-        // TODO: Нужен способ сказать, что некоторые времена одинаково приоритетны.
-        // Например, как выразить, что в любой день недели первой парой - норм, второй - менее удобно, а в любое другое время - не удобно.
-        public MeetingTime[] MeetingTimePriorities;
+        public MeetingTimeRequesition[] MeetingTimePriorities;
         public Teacher Teacher;
 
-        public Requisition(LearningPlanItem planItem, GroupRequisition[] groupPriorities, string? location, int repetitionsCount, MeetingTime[] meetingTimePriorities, Teacher teacher)
+        public Requisition(LearningPlanItem planItem, GroupRequisition[] groupPriorities, string location, int repetitionsCount, MeetingTimeRequesition[] meetingTimePriorities, Teacher teacher)
         {
             PlanItem = planItem;
             GroupPriorities = groupPriorities;
@@ -38,11 +25,31 @@ namespace ScheduleLib
 
     public class GroupRequisition
     {
+        public GroupsChoice[] GroupsChoices;
+
+        public GroupRequisition(GroupsChoice[] groupsChoices)
+        {
+            GroupsChoices = groupsChoices;
+        }
+    }
+
+    public class MeetingTimeRequesition
+    {
+        public MeetingTime[] MeetingTimeChoices;
+
+        public MeetingTimeRequesition(MeetingTime[] meetingTimeChoices)
+        {
+            MeetingTimeChoices = meetingTimeChoices;
+        }
+    }
+
+    public class GroupsChoice
+    {
         public string[] Groups;
 
-        public GroupRequisition(string priority)
+        public GroupsChoice(string[] groups)
         {
-            Groups = priority.Split('+').Select(s => s.Trim()).ToArray();
+            Groups = groups;
         }
     }
 }
