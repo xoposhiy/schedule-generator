97323ac442739e8b1f9be90639eb2cc11abc367b
KachusovStepan <stepanurfu@gmail.com>
2020-12-25 13:21:20 +0500

    Added RequisitionToMeetingConverter and test for it

---
+++ ScheduleGenerator/Domain/Conversions/RequistionToMeetingConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.ScheduleLib;
+
+
+namespace Domain.Conversions
+{
+    public static class RequistionToMeetingConverter
+    {
+        public static List<HashSet<Meeting>> ConvertRequistionToMeetingWithoutTime(Requisition requisition)
+        {
+            var discipline = requisition.PlanItem.Discipline;
+            var meetingType = requisition.PlanItem.MeetingType;
+
+            var meetingByPriorityList = new List<HashSet<Meeting>>();
+            foreach (var groupRequisition in requisition.GroupPriorities)
+            {
+                var meetingsOfPriority = new HashSet<Meeting>();
+                foreach (var groupChoices in groupRequisition.GroupsChoices)
+                {
+                    for (int i = 0; i < requisition.RepetitionsCount; i++)
+                    {
+                        var meeting = new Meeting(discipline, meetingType, groupChoices.Groups);
+                        meeting.Location = requisition.Location;
+                        meetingsOfPriority.Add(meeting);
+                    }
+                }
+                meetingByPriorityList.Add(meetingsOfPriority);
+            }
+            return meetingByPriorityList;
+        }
+    }
+}
---
+++ ScheduleGenerator/Testing/ConversionsTests/RequistionToMeetingConverterTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+using Domain.ScheduleLib;
+using Domain.Conversions;
+
+
+namespace ConversionsTests
+{
+    class RequistionToMeetingConverterTest
+    {
+        [Test]
+        public void ConvertRequistionToMeetingWithoutTimeCorrect()
+        {
+            var learningPlanItem = new LearningPlanItem("FT-202", new Discipline("OOP"), MeetingType.Seminar, GroupSize.FullGroup, 2);
+            var requisition = new Requisition(
+                learningPlanItem,
+                new[] { new GroupRequisition(new[] { new GroupsChoice(new[] { new MeetingGroup("FT-202", GroupPart.FullGroup) }) }) },
+                "150",
+                2,
+                new[] { new MeetingTimeRequesition(new[] { new MeetingTime(DayOfWeek.Tuesday, 2) }) },
+                new Teacher("Pavel Egorov"));
+            var meetings = RequistionToMeetingConverter.ConvertRequistionToMeetingWithoutTime(requisition);
+            Assert.AreEqual(1, meetings.Count);
+            Assert.AreEqual(2, meetings[0].Count);
+            var actualMeeting = meetings[0].First();
+            Assert.AreEqual("OOP", actualMeeting.Discipline.Name);
+            Assert.AreEqual(1, actualMeeting.Groups.Length);
+            Assert.AreEqual("FT-202", actualMeeting.Groups[0].GroupName);
+            Assert.AreEqual(GroupPart.FullGroup, actualMeeting.Groups[0].GroupPart);
+            Assert.AreEqual("150", actualMeeting.Location);
+        }
+    }
+}
