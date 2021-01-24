cb56ad4e3a26d50e4b398fc581688ca0575d792e
KachusovStepan <stepanurfu@gmail.com>
2021-01-25 00:49:12 +0500

    added class for performing evaluation of several rules in one evaluate call

---
+++ ScheduleGenerator/Domain/Rules/MeetingEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain.ScheduleLib;
+using System.Collections.Generic;
+
+
+namespace Domain.Rules
+{
+    public class MeetingEvaluator
+    {
+        private IEnumerable<IRule> rules;
+        public MeetingEvaluator(IEnumerable<IRule> rules)
+        {
+            this.rules = rules;
+        }
+
+        public double Evaluate(LearningPlan learningPlan, Requisition requisition, Schedule schedule, Meeting meetingToAdd)
+        {
+            var totalPenalty = 0d;
+            foreach (var rule in rules)
+            {
+                totalPenalty += rule.Evaluate(learningPlan, requisition, schedule, meetingToAdd);
+            }
+
+            return totalPenalty;
+        }
+    }
+}
