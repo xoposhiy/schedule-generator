84a5d65ea718f7ff164d8b8eded0dcfe6f27798e
Teewar <teewar25@gmail.com>
2021-01-25 01:12:46 +0500

    add IScheduleGenerator

---
+++ ScheduleGenerator/Domain/Algorithms/GreedyScheduleGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+using Domain.ScheduleLib;
+
+namespace Domain.Algorithms
+{
+    public class GreedyScheduleGenerator : IScheduleGenerator
+    {
+        public Schedule MakeSchedule(LearningPlan learningPlan, Requisition requisition)
+        {
+            var meetingsToFill =
+                Conversions.RequistionToMeetingConverter.ConvertRequistionToMeetingWithoutTime(requisition).ToArray();
+            var bestSchedule = new Schedule(new Meeting[0]);
+            var bestPenalty = double.PositiveInfinity;
+            for (var i = 0; i < 100; ++i)
+            {
+                var shuffledMeetingsToFill = meetingsToFill.Shuffled();
+                var tryingResult = TryMakeSchedule(learningPlan, shuffledMeetingsToFill, requisition);
+                if (bestPenalty > tryingResult.Item2)
+                {
+                    bestPenalty = tryingResult.Item2;
+                    bestSchedule = tryingResult.Item1;
+                }
+            }
+
+            return bestSchedule;
+        }
+
+        public (Schedule, double) TryMakeSchedule(LearningPlan learningPlan, Meeting[] meetingsToFill, Requisition requisition)
+        {
+            return (new Schedule(new Meeting[0]), 0);
+        }
+    }
+}
---
+++ ScheduleGenerator/Domain/Algorithms/IScheduleGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Domain.ScheduleLib;
+
+namespace Domain.Algorithms
+{
+    public interface IScheduleGenerator
+    {
+        public Schedule MakeSchedule(LearningPlan learningPlan, Requisition requisition);
+    }
+}
