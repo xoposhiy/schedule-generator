9dd28fe398e8d456e6fe89fdcaeb9540af2627b7
KachusovStepan <stepanurfu@gmail.com>
2021-01-24 21:12:20 +0500

    deleted comments with previous code

--- ScheduleGenerator/Domain/Rules/NoMoreThanOneMeetingAtTimeForLocationRule.cs
+++ ScheduleGenerator/Domain/Rules/NoMoreThanOneMeetingAtTimeForLocationRule.cs
@@ -39,49 +39,5 @@ namespace Domain.Rules
 
             return new List<Meeting>();
         }
-
-        //public EvaluationResult Evaluate(Schedule schedule, Requisition requisition)
-        //{
-        //    var badMeetings = GetBadMeetings(schedule);
-        //    return new EvaluationResult
-        //    (
-        //        badMeetings.Length > 0 ? UnitPenalty : 0,
-        //        badMeetings,
-        //        "Нельзя проводить несколько пар в одном месте одновременно"
-        //    );
-        //}
-
-        //private static Meeting[] GetBadMeetings(Schedule schedule)
-        //{
-        //    var badMeetings = new List<Meeting>();
-        //    foreach (var grouping in schedule.Meetings.GroupBy(meeting => meeting.Location))
-        //    {
-        //        if(grouping.Key == "Online")
-        //        {
-        //            continue;
-        //        }
-        //        var sortedByTimeMeetings = new Dictionary<MeetingTime, List<Meeting>>();
-        //        foreach (var meeting in grouping)
-        //        {
-        //            if (meeting.MeetingTime is null)
-        //            {
-        //                continue;
-        //            }
-        //            if (!sortedByTimeMeetings.ContainsKey(meeting.MeetingTime))
-        //            {
-        //                sortedByTimeMeetings[meeting.MeetingTime] = new List<Meeting>();
-        //            }
-        //            sortedByTimeMeetings[meeting.MeetingTime].Add(meeting);
-        //        }
-        //        foreach (var sameTimeMeetings in sortedByTimeMeetings.Values)
-        //        {
-        //            if (sameTimeMeetings.Count > 1)
-        //            {
-        //                badMeetings = badMeetings.Concat(sameTimeMeetings).ToList();
-        //            }
-        //        }
-        //    }
-        //    return badMeetings.ToArray();
-        //}
     }
 }
--- ScheduleGenerator/Domain/Rules/NoMoreThanOneMeetingAtTimeForTeacherRule.cs
+++ ScheduleGenerator/Domain/Rules/NoMoreThanOneMeetingAtTimeForTeacherRule.cs
@@ -34,45 +34,5 @@ namespace Domain.Rules
 
             return new List<Meeting>();
         }
-
-        //public EvaluationResult Evaluate(Schedule schedule, Requisition requisition)
-        //{
-        //    var badMeetings = GetBadMeetings(schedule);
-        //    return new EvaluationResult
-        //    (
-        //        badMeetings.Length * UnitPenalty,
-        //        badMeetings,
-        //        "Преподаватель не может проводить более чем одну пару одновременно"
-        //    );
-        //}
-
-        //private static Meeting[] GetBadMeetings(Schedule schedule)
-        //{
-        //    var badMeetings = new List<Meeting>();
-        //    foreach (var grouping in schedule.Meetings.GroupBy(meeting => meeting.Teacher))
-        //    {
-        //        var sortedByTimeMeetings = new Dictionary<MeetingTime, List<Meeting>>();
-        //        foreach (var meeting in grouping)
-        //        {
-        //            if (meeting.MeetingTime is null)
-        //            {
-        //                continue;
-        //            }
-        //            if (!sortedByTimeMeetings.ContainsKey(meeting.MeetingTime))
-        //            {
-        //                sortedByTimeMeetings[meeting.MeetingTime] = new List<Meeting>();
-        //            }
-        //            sortedByTimeMeetings[meeting.MeetingTime].Add(meeting);
-        //        }
-        //        foreach (var sameTimeMeetings in sortedByTimeMeetings.Values)
-        //        {
-        //            if (sameTimeMeetings.Count > 1)
-        //            {
-        //                badMeetings = badMeetings.Concat(sameTimeMeetings).ToList();
-        //            }
-        //        }
-        //    }
-        //    return badMeetings.ToArray();
-        //}
     }
 }
