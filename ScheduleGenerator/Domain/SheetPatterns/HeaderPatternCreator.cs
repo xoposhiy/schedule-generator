da38e99b189f2e390b52443b37a05bef6f83a2f0
KachusovStepan <stepanurfu@gmail.com>
2021-01-24 23:41:20 +0500

    added sheet header pattern creator (with comments) and test for it.

---
+++ ScheduleGenerator/Domain/SheetPatterns/HeaderPatternCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Apis.Sheets.v4.Data;
+
+using Domain.GoogleSheetsRepository;
+
+
+namespace Domain.SheetPatterns
+{
+    public class HeaderPatternCreator
+    {
+        public static void SetUpHeaders(GSRepository repo, string sheetName,
+                (int row, int col) start, List<string> headers, List<string> comments = null)
+        {
+            var modifier = repo
+                    .ModifySpreadSheet(sheetName);
+            for (int i = 0; i < headers.Count; i++)
+            {
+                modifier
+                    .WriteRange((start.row, start.col + i), new List<List<string>>() { new List<string>() { headers[i] } })
+                    .AddBorders((start.row, start.col + i), (start.row, start.col + i), new Color() { Blue = 1 });
+                if (comments != null && i < comments.Count)
+                {
+                    modifier.AddComment((start.row, start.col + i), comments[i]);
+                }
+            }
+            modifier.Execute();
+        }
+    }
+}
