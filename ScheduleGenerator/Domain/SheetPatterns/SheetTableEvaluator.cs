8e8bafd111f5d54e555c304118694e409c84deb4
KachusovStepan <stepanurfu@gmail.com>
2021-01-25 12:53:51 +0500

    added sheet evaluation according to col patterns and error output to sheet (color and comments), added tests for these classes

---
+++ ScheduleGenerator/Domain/SheetPatterns/SheetTableEvaluator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Domain.SheetPatterns
+{
+    public class SheetTableEvaluator
+    {
+        private List<(Regex regex, string msg)> regexMsgList;
+        public SheetTableEvaluator(List<(string, string)> patternMsgDict)
+        {
+            regexMsgList = SetUpRegexes(patternMsgDict);
+        }
+
+        private List<(Regex, string)> SetUpRegexes(List<(string pattern, string msg)> patternMsgList)
+        {
+            var regexMsgDict = new List<(Regex, string)>();
+            foreach (var patternMsg in patternMsgList)
+            {
+                var regex = new Regex(patternMsg.pattern);
+                regexMsgDict.Add((regex, patternMsg.msg));
+            }
+
+            return regexMsgDict;
+        }
+
+        public List<((int, int), string)> Evaluate(List<List<string>> tableData, (int rowIndex, int colIndex) start)
+        {
+            var issues = new List<((int, int), string)>();
+            for (var r = start.rowIndex; r < tableData.Count; r++)
+            {
+                var currentRow = tableData[r];
+                var rowCount = currentRow.Count;
+                for (var c = 0; c < rowCount; c++)
+                {
+
+                    if (c < regexMsgList.Count)
+                    {
+                        var column = c + start.colIndex;
+                        var cellData = currentRow[column];
+                        if (!regexMsgList[c].regex.IsMatch(cellData))
+                        {
+                            issues.Add(((r, column), regexMsgList[c].msg));
+                        }
+                    }
+                }
+            }
+
+            return issues;
+        }
+    }
+}
---
+++ ScheduleGenerator/Testing/SheetPatternsTest/SheetTableErrorPainterTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Domain.SheetPatterns;
+using Domain.GoogleSheetsRepository;
+
+
+namespace SheetPatternsTest
+{
+    class SheetTableErrorPainterTest
+    {
+        private static string CredentialsEnvVar = "GoogleApiCredentials";
+        private static readonly string ApplicationName = "MyApp";
+        private static string url = "https://docs.google.com/spreadsheets/d/1JxL_CTuc-NLaBRdY5F4vz3yn6WJe8bp-7Mn7ViycjtQ/edit#gid=566045364";
+        private static string sheetName = "SheetTablePainterPractice";
+
+        [Test]
+        public void PaintErrorsWhenNoMergerCells()
+        {
+            var credentialDirPath = Environment.GetEnvironmentVariable(CredentialsEnvVar);
+            var credentialPath = credentialDirPath + "\\client_secrets.json";
+            var repo = new GSRepository(ApplicationName, credentialPath, url);
+
+
+            var errors = new List<((int, int), string)>() {
+                ((0, 0), "error msg1"), ((1, 1), "error msg2"), ((4, 2), "error msg3")
+            };
+
+            SheetTableErrorPainter.PaintErrors(repo, sheetName, (1, 0), errors);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void PaintErrorsAndClear()
+        {
+            var credentialDirPath = Environment.GetEnvironmentVariable(CredentialsEnvVar);
+            var credentialPath = credentialDirPath + "\\client_secrets.json";
+            var repo = new GSRepository(ApplicationName, credentialPath, url);
+
+            var errors = new List<((int, int), string)>() {
+                ((0, 0), "error msg1"), ((1, 1), "error msg2"), ((4, 2), "error msg3")
+            };
+            SheetTableErrorPainter.PaintErrors(repo, sheetName, (5, 0), errors);
+
+            var coordsToClear = errors.Select(e => e.Item1);
+            SheetTableErrorPainter.ClearErrorPaint(repo, sheetName, (5, 0), coordsToClear);
+
+            Assert.Pass();
+        }
+    }
+}
