835d8855a705f812b5acea5616c75c300dd3af16
krutovsky <krutovsky.danya@gmail.com>
2021-09-17 13:58:18 +0500

    Filling RequisitionTests.cs

--- ScheduleGenerator/Testing/ScheduleLibTests/RequisitionTests.cs
+++ ScheduleGenerator/Testing/ScheduleLibTests/RequisitionTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Domain.ScheduleLib;
+using NUnit.Framework;
 
 namespace Testing.ScheduleLibTests
 {
@@ -8,8 +10,23 @@ namespace Testing.ScheduleLibTests
         [Test]
         public void TestGroupMaskInit()
         {
-            //var priority = new GroupRequisition("a + b + c + d");
-            //Assert.AreEqual(new[] { "a", "b", "c", "d" }, priority.Groups);
+            var discipline = new Discipline("Test");
+            var roomSpecs = new[] {RoomSpec.Big, RoomSpec.Projector};
+            var planItem = new LearningPlanItem("ФИИТ-666", discipline, MeetingType.Lecture, GroupSize.FullGroup, 1,
+                roomSpecs, null, null);
+            
+            var meetingGroup = new MeetingGroup("Test", GroupPart.FullGroup);
+            var groupChoice = new GroupsChoice(new[] {meetingGroup});
+            var groupRequisition = new GroupRequisition(new[] {groupChoice});
+            
+            var metingTime = new MeetingTime(DayOfWeek.Monday, 2);
+            var meetingTimeRequisition = new MeetingTimeRequisition(new[] {metingTime});
+            
+            var teacher = new Teacher("God");
+            
+            var requisitionItem = new RequisitionItem(planItem, new[] {groupRequisition}, 1,
+                new[] {meetingTimeRequisition}, teacher, WeekType.All);
+            
             Assert.Pass();
         }
     }
