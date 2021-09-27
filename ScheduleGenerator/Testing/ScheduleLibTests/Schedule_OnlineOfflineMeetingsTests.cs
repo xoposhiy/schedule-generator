5c9abc13a7ecebd8ad641d9decfaf15d976d9eb0
krutovsky <krutovsky.danya@gmail.com>
2021-09-27 13:15:06 +0500

    TestOnlineOfflineGap

--- ScheduleGenerator/Testing/ScheduleLibTests/Schedule_OnlineOfflineMeetingsTests.cs
+++ ScheduleGenerator/Testing/ScheduleLibTests/Schedule_OnlineOfflineMeetingsTests.cs
@@ -14,7 +14,7 @@ namespace Testing.ScheduleLibTests
             var requisition = new Requisition(new[]
             {
                 new RequisitionItem(ObjectMother.CalculusLecture, "ФИИТ-101", "пн 1-6 пара",
-                    ObjectMother.CalculusTeacher, 1, WeekType.All, true),
+                    ObjectMother.CalculusTeacher, 1, isOnline: true),
                 new RequisitionItem(ObjectMother.CalculusSeminar, "ФИИТ-101", "пн 1-6 пара",
                     ObjectMother.CalculusTeacher),
             });
@@ -30,6 +30,31 @@ namespace Testing.ScheduleLibTests
                 .Select(m => m.Location);
             Assert.That(lecturePossibleLocations, Has.All.EqualTo(Meeting.OnlineLocationName));
         }
-        //TODO: Проверить, что между онлайн парами и оффлайн достаточно перерыва
+
+        [Test]
+        public void TestOnlineOfflineGap()
+        {
+            var requisition = new Requisition(new[]
+            {
+                new RequisitionItem(ObjectMother.CalculusLecture, "ФИИТ-101", "пн 1-2 пара",
+                    ObjectMother.CalculusTeacher, 1, isOnline: true),
+                new RequisitionItem(ObjectMother.CalculusSeminar, "ФИИТ-101", "пн 1-6 пара",
+                    ObjectMother.CalculusTeacher),
+            });
+            var schedule = new Schedule(requisition, ObjectMother.ClassRooms);
+
+            var lecture = schedule
+                .GetMeetingsToAdd()
+                .Where(m => m.MeetingType == MeetingType.Lecture)
+                .First(m => m.MeetingTime!.TimeSlotIndex == 1);
+            schedule.AddMeeting(lecture);
+            Console.Error.WriteLine(lecture);
+
+            foreach (var meeting in schedule.GetMeetingsToAdd().Where(m => m.MeetingType == MeetingType.Seminar))
+            {
+                Console.Error.WriteLine(meeting);
+                Assert.Greater(meeting.MeetingTime!.TimeSlotIndex - lecture.MeetingTime!.TimeSlotIndex, 1);
+            }
+        }
     }
 }
