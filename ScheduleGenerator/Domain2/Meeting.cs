a976f61de2117336bf636389ae2a1beeb5da8675
krutovsky-danya <krutovsky.danya@gmail.com>
2022-03-26 22:23:47 +0500

    Hot fix

--- ScheduleGenerator/Domain2/Meeting.cs
+++ ScheduleGenerator/Domain2/Meeting.cs
@@ -3,19 +3,18 @@ using CommonDomain.Enums;
 
 namespace Domain2;
 
-public class Meeting
+public record Meeting
 {
     public readonly MeetingType? After;
     public readonly int AmountPerWeek;
     public readonly string? Classroom;
     public readonly Discipline Discipline;
     public readonly int Duration;
-    public readonly int GroupIndex;
-    public readonly bool HasTests;
+    public readonly List<int> Groups;
+    public readonly bool HasEntranceTest;
     public readonly Location Location;
     public readonly List<MeetingTime> MeetingTimePriorities;
     public readonly RoomSpec[] RoomSpecs;
     public readonly Teacher Teacher;
-    public readonly WeekType WeekType;
     public bool ShouldBePlaced => AmountPerWeek != 0;
 }
