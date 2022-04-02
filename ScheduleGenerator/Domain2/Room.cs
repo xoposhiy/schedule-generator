844acd7400b5e9bb66fa0bd140a35a057f232849
Andrey135296 <32587274+Andrey135296@users.noreply.github.com>
2022-04-03 01:16:35 +0500

    adding parsing 2

---
+++ ScheduleGenerator/Domain2/Room.cs
@@ -0,0 +1,5 @@
+using CommonDomain.Enums;
+
+namespace Domain2;
+
+public record Room(Location Location, List<RoomSpec> RoomSpecs, HashSet<MeetingTime> locked);
--- ScheduleGenerator/Domain2/SheetToRequisitionConverter.cs
+++ ScheduleGenerator/Domain2/SheetToRequisitionConverter.cs
@@ -5,33 +5,61 @@ using CommonDomain.Enums;
 using Infrastructure.GoogleSheetsRepository;
 using Infrastructure.SheetPatterns;
 
-namespace Domain2
+namespace Domain2;
+
+public static class SheetToRequisitionConverter
 {
-    public static class SheetToRequisitionConverter
+    public static readonly Dictionary<string, DayOfWeek> WeekDaysDict = new()
     {
-        public static readonly Dictionary<string, DayOfWeek> WeekDaysDict = new()
+        {"пн", DayOfWeek.Monday},
+        {"вт", DayOfWeek.Tuesday},
+        {"ср", DayOfWeek.Wednesday},
+        {"чт", DayOfWeek.Thursday},
+        {"пт", DayOfWeek.Friday},
+        {"сб", DayOfWeek.Saturday}
+    };
+
+    public static Location StringToLocation(string place) => new Location(place);
+
+    private static MeetingType GetMeetingType(string rowMeetingType)
+    {
+        return rowMeetingType switch
         {
-            {"пн", DayOfWeek.Monday},
-            {"вт", DayOfWeek.Tuesday},
-            {"ср", DayOfWeek.Wednesday},
-            {"чт", DayOfWeek.Thursday},
-            {"пт", DayOfWeek.Friday},
-            {"сб", DayOfWeek.Saturday}
+            "Лекция" => MeetingType.Lecture,
+            "КомпПрактика" => MeetingType.ComputerLab,
+            "Семинар" => MeetingType.Seminar,
+            _ => throw new FormatException($"Некорректный тип занятия: {rowMeetingType}")
         };
-
-        public static Location StringToLocation(string place) => new Location(place);
-
-        private static MeetingType GetMeetingType(string rowMeetingType)
-        {
-            return rowMeetingType switch
-            {
-                "Лекция" => MeetingType.Lecture,
-                "КомпПрактика" => MeetingType.ComputerLab,
-                "Семинар" => MeetingType.Seminar,
-                _ => throw new FormatException($"Некорректный тип занятия: {rowMeetingType}")
-            };
-        }
-
-        
     }
+
+    private static WeekType GetWeekType(string rowWeekType)
+    {
+        return rowWeekType switch
+        {
+            "чет" => WeekType.Even,
+            "нечет" => WeekType.Odd,
+            _ => throw new FormatException($"Некорректная четность недели: {rowWeekType}")
+        };
+    }
+
+    private static RoomSpec GetRoomSpec(string rowRoomSpec)
+    {
+        return rowRoomSpec switch
+        {
+            "компьютеры" => RoomSpec.Computer,
+            "проектор" => RoomSpec.Projector,
+            "большая" => RoomSpec.Big,
+            "на группу" => RoomSpec.ForGroup,
+            _ => throw new FormatException($"Некорректный тип аудитории: {rowRoomSpec}")
+        };
+    }
+
+    public static (List<Meeting2>, List<Room>)
+        ConvertToRequisitions(GsRepository repo,
+            string requisitionSheetName, string learningPlanSheetName, string classroomsSheetName)
+    {
+        throw new NotImplementedException();
+    }
+
+    
 }
