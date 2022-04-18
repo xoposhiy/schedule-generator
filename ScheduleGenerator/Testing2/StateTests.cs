6543465f7dc5a0e213c22f5ad805a2e64f1a7e68
krutovsky-danya <krutovsky.danya@gmail.com>
2022-04-19 00:00:20 +0500

    Init tests

---
+++ ScheduleGenerator/Testing2/StateTests.cs
@@ -0,0 +1,14 @@
+﻿using Domain2;
+using NUnit.Framework;
+
+namespace Testing2;
+
+public static class StateTests
+{
+    [Test]
+    public static void AddMeetingTest()
+    {
+        // var meeting = new Meeting2();
+        var state = new State(Array.Empty<Meeting2>());
+    }
+}
