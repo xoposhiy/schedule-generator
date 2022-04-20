05b182392141e3903babf24416cef575b8acb75d
Timofey Belov <tvb060201@gmail.com>
2022-04-20 23:55:48 +0500

    Stupid function added

--- ScheduleGenerator/Domain2/ProbabilityStorage.cs
+++ ScheduleGenerator/Domain2/ProbabilityStorage.cs
@@ -15,16 +15,39 @@ public static class ProbabilityStorage
         if (!StudentWithDisciplineToPriority.ContainsKey(student))
             StudentWithDisciplineToPriority.Add(student, new Dictionary<Discipline, int>());
         StudentWithDisciplineToPriority[student].Add(discipline, priority);
-        
+
         if (!DisciplineWithPriorityToStudents.ContainsKey(discipline))
-            DisciplineWithPriorityToStudents.Add(discipline, Enumerable.Range(0,5)
+            DisciplineWithPriorityToStudents.Add(discipline, Enumerable.Range(0, 5)
                 .Select(_ => new HashSet<string>()).ToList());
         DisciplineWithPriorityToStudents[discipline][priority].Add(student);
     }
 
     public static double GetCommonStudents(this Meeting2 firstMeeting, Meeting2 secondMeeting)
     {
-        return new Random().NextDouble();
+        var firstDiscipline = firstMeeting.Discipline;
+        var secondDiscipline = secondMeeting.Discipline;
+        var firstGroups = firstMeeting.Groups;
+        var secondGroups = secondMeeting.Groups;
+        var students1 = DisciplineWithPriorityToStudents[firstDiscipline];
+        var students2 = DisciplineWithPriorityToStudents[secondDiscipline];
+        var intersections = new List<HashSet<string>>();
+        for (var prior = 0; prior < students1.Count; prior++)
+        {
+            var elem = students1[prior].ToHashSet();
+            elem.IntersectWith(students2[prior]);
+            intersections.Add(elem);
+        }
+
+        if (firstDiscipline.Type == secondDiscipline.Type)
+        {
+            if (firstDiscipline.Type == DisciplineType.WithEntranceTest)
+                return intersections.Select((students, priority) =>
+                    students.Count * PriorityWithEntranceToProbability[priority]).Sum();
+            return intersections.Select((students, priority) =>
+                students.Count * PriorityCommonToProbability[priority]).Sum();
+        }
+
+        return intersections.Select((students, priority) =>
+            students.Count * (PriorityCommonToProbability[priority] + PriorityCommonToProbability[priority]) / 2).Sum();
     }
-    
 }
