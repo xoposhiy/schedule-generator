a0e080c11ed99711597e6bb923f28dfc893c51af
krutovsky-danya <krutovsky.danya@gmail.com>
2022-06-05 14:03:18 +0500

    refactoring to better understand class

--- ScheduleGenerator/Domain2/SheetToProbabilityConverter.cs
+++ ScheduleGenerator/Domain2/SheetToProbabilityConverter.cs
@@ -3,55 +3,43 @@ using CommonInfrastructure;
 using CommonInfrastructure.GoogleSheetsRepository;
 using static Domain2.Constants;
 
-// ReSharper disable MemberCanBePrivate.Global
-
 namespace Domain2;
 
 public static class SheetToProbabilityConverter
 {
     private const int StartColumn = 2;
-    private static int DisciplinesCount;
-    private static int RightBorder;
-    private static readonly Dictionary<int, Discipline> IndexToDiscipline = new();
+    private static int disciplinesCount;
+    private static int rightBorder;
 
     public static void SetDisciplinesCount(int count)
     {
-        DisciplinesCount = count;
-        RightBorder = DisciplinesCount * 3 + StartColumn;
+        disciplinesCount = count;
+        rightBorder = disciplinesCount * 3 + StartColumn;
     }
 
     public static void ReadPriorities(GsRepository repo, ProbabilityStorage probabilityStorage,
-        IEnumerable<Meeting2> meetings, string meetingsSheetName)
+        IList<Meeting2> meetings, string meetingsSheetName)
     {
         probabilityStorage.FillDisciplineToMaxGroups(meetings);
 
         var disciplines = meetings.Select(e => e.Discipline).ToHashSet();
         var prioritiesDataRaw = SheetTableReader.ReadRowsFromSheet(repo, meetingsSheetName,
-            0, 0, RightBorder);
+            0, 0, rightBorder);
         var header = prioritiesDataRaw[0];
+        var indexToDiscipline = new Dictionary<int, Discipline>();
         for (var i = StartColumn; i < header.Count; i += 3)
-            IndexToDiscipline[i] = disciplines.First(d => d.Name == header[i]);
+            indexToDiscipline[i] = disciplines.First(d => d.Name == header[i]);
 
         foreach (var row in prioritiesDataRaw.Skip(2))
         {
             var student = row[0];
-            switch (student)
+            if (string.IsNullOrEmpty(student)) continue;
+            for (var i = StartColumn; i < rightBorder; i += 3)
             {
-                case "":
-                {
-                    break;
-                }
-                default:
-                {
-                    for (var i = StartColumn; i < RightBorder; i += 3)
-                    {
-                        var priority = ParseInt(row[i], UnselectedPriority);
-                        // int.TryParse(row[i+1], out var priorityLk);
-                        probabilityStorage.AddSubjectForStudent(student, IndexToDiscipline[i], priority);
-                    }
-
-                    break;
-                }
+                var priority = ParseInt(row[i], UnselectedPriority);
+                // int.TryParse(row[i + 1], out var priorityLk);
+                // var isChosen = row[i + 2] == "Зачислен";
+                probabilityStorage.AddSubjectForStudent(student, indexToDiscipline[i], priority);
             }
         }
     }
