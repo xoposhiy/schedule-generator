9a947c07dd2c15a83657ca13d374a4ae8bf6b7b2
MexBanDoc <tvb060201@gmail.com>
2022-08-20 14:01:57 +0500

    Helper added

---
+++ ScheduleGenerator/Domain2/Converters/ParsingHelper.cs
@@ -0,0 +1,38 @@
+﻿using CommonInfrastructure.GoogleSheetsRepository;
+
+namespace Domain2.Converters;
+
+public class ParsingHelper
+{
+    private TermType termType;
+    private bool isFinal;
+    private SourcePrioritiesType sourcePrioritiesType;
+    private GsRepository repo;
+
+    public ParsingHelper(TermType termType, bool isFinal, SourcePrioritiesType sourcePrioritiesType, GsRepository repo)
+    {
+        this.termType = termType;
+        this.isFinal = isFinal;
+        this.sourcePrioritiesType = sourcePrioritiesType;
+        this.repo = repo;
+    }
+    
+
+    public ProbabilityStorage ParseProbabilities()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum SourcePrioritiesType
+{
+    GoogleSheet = 0,
+    JsonLk = 1,
+    JsonFinal = 2
+}
+
+public enum TermType
+{
+    Autumn = 0,
+    Spring = 1
+}
