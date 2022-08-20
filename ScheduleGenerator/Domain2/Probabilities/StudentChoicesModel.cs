a7441405b740975bc7e0d2ece63184bddba3fdd0
MexBanDoc <tvb060201@gmail.com>
2022-08-20 12:11:55 +0500

    Added basic json distribution classes

---
+++ ScheduleGenerator/Domain2/Probabilities/StudentChoicesModel.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+#pragma warning disable CS8618
+
+namespace Domain2.Probabilities;
+
+[JsonObject]
+public class StudentChoicesModel
+{ 
+    public ulong PersonalNumber { get; set; }
+    
+    [JsonProperty("fullname")]
+    public string FullName { get; set; }
+    
+    [JsonProperty("group")]
+    public string GroupId { get; set; }
+    
+    public List<string> MupIds { get; set; }
+}
+
+[JsonObject]
+public class StudentsDistribution
+{
+    public List<StudentChoicesModel> Students { get; set; }
+    public Dictionary<Guid, string> MupIdToMupName { get; set; }
+}
